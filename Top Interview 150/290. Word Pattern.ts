abe7e96c7a1331c0cb4022f9b637711a156f737b
trond1n <lopata@2ch.hk>
2025-03-13 11:16:58 +0500

    290. Word Pattern

---
+++ Top Interview 150/290. Word Pattern.ts
@@ -0,0 +1,28 @@
+// 290. Word Pattern
+// Даны шаблон и строка s, определите, следует ли s тому же шаблону.
+
+// Здесь "следует" означает полное совпадение, такое, что существует биекция между буквой в шаблоне и непустым словом в s. В частности:
+
+// Каждая буква в шаблоне соответствует ровно одному уникальному слову в s.
+// Каждое уникальное слово в s соответствует ровно одной букве в шаблоне.
+// Никакие две буквы не соответствуют одному и тому же слову, и никакие два слова не соответствуют одной и той же букве.
+
+function wordPattern(pattern: string, s: string): boolean {
+    const hash = new Map();
+    const words = s.split(' ');
+    if (pattern.length !== words.length) return false;
+    for (let i = 0; i < pattern.length; i++) {
+
+        const patternChar = pattern[i]; //а
+        const word = words[i];//dog
+        if (!hash.has(patternChar)) //а нет в мапе
+        {
+            hash.set(patternChar, word);
+        } else {
+            let hashChar = hash.get(patternChar);
+            if (hashChar !== word) return false;
+        }
+    }
+    return true;
+};
+console.log(wordPattern('abba', "dog dog dog dog"));
